84b2373e4ffbaaaeffe29ffe7535c65832318495
Pravin <pravinkumar2108@gmail.com>
2024-09-23 18:29:59 +0530

    course-id progress

--- course/views.py
+++ course/views.py
@@ -1,3 +1,21 @@
+import json
+import os
+from django.conf import settings
 from django.shortcuts import render
 
-# Create your views here.
+
+def course_list(request, *args, **kwargs):
+    base_dir = settings.BASE_DIR
+    file_path = os.path.join(base_dir, "api", "get_all_courses_API_response.json")
+    try:
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            courses = data.get("courses", [])
+    except (FileNotFoundError, json.JSONDecodeError):
+        courses = []
+
+    return render(
+        request,
+        "course-list/course-list.html",
+        {"courses": courses},
+    )
