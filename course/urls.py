d0d1f0e89387baf9ca8ce7c590dcfbab7ffd90b9
Pravin <pravinkumar2108@gmail.com>
2024-09-24 03:04:18 +0530

    course page-2

--- course/urls.py
+++ course/urls.py
@@ -6,5 +6,5 @@ app_name = "course"
 
 urlpatterns = [
     path("", views.course_list, name="course_list"),
-    # path("course/<int:course_id>/", views.course_detail, name="course_detail"),
+    path("<int:course_id>/", views.course_detail_list, name="course_detail"),
 ]
--- course/views.py
+++ course/views.py
@@ -19,3 +19,21 @@ def course_list(request, *args, **kwargs):
         "course-list/course-list.html",
         {"courses": courses},
     )
+
+
+def course_detail_list(request, *args, **kwargs):
+
+    course_id = kwargs.get("course_id")
+    base_dir = settings.BASE_DIR
+    file_path = os.path.join(base_dir, "api", "get_course_detail_API_response.json")
+    try:
+        with open(file_path, "r") as file:
+            data = json.load(file)
+            if str(data.get("course_id")) == str(course_id):
+                videos = data.get("videos", [])
+            else:
+                print(f"Course ID {course_id} not found in JSON")
+    except (FileNotFoundError, json.JSONDecodeError) as e:
+        print(f"Error loading JSON file: {e}")
+        videos = []
+    return render(request, "course-detail/course_details.html", {"videos": videos})
