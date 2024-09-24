8c73b516b1be3eeb8ca983f92de7e7fb59c90638
Pravin <pravinkumar2108@gmail.com>
2024-09-24 13:04:58 +0530

    page-1 and page-2 complete

--- course/views.py
+++ course/views.py
@@ -2,6 +2,7 @@ import json
 import os
 from django.conf import settings
 from django.shortcuts import render
+import re
 
 
 def course_list(request, *args, **kwargs):
@@ -21,19 +22,52 @@ def course_list(request, *args, **kwargs):
     )
 
 
-def course_detail_list(request, *args, **kwargs):
+def generate_embed_urls(youtube_urls):
+    embed_urls = []
+    for url in youtube_urls:
+        video_id_match = re.search(
+            r"(v=|/vi?/|youtu\.be/|\/embed\/|\/shorts\/)([^&?\/]+)", url
+        )
+        if video_id_match:
+            video_id = video_id_match.group(2)
+            embed_url = f"https://www.youtube.com/embed/{video_id}"
+            embed_urls.append(embed_url)
+        else:
+            embed_urls.append("Invalid YouTube URL")
+    return embed_urls
 
+
+def course_detail_list(request, *args, **kwargs):
+    videos = []
+    yt_urls = []
+    embed_urls = []
     course_id = kwargs.get("course_id")
     base_dir = settings.BASE_DIR
     file_path = os.path.join(base_dir, "api", "get_course_detail_API_response.json")
+
     try:
         with open(file_path, "r") as file:
             data = json.load(file)
             if str(data.get("course_id")) == str(course_id):
                 videos = data.get("videos", [])
+
+                yt_urls = [
+                    video.get("youtube_url")
+                    for video in videos
+                    if "youtube_url" in video
+                ]
+                embed_urls = generate_embed_urls(yt_urls)
             else:
                 print(f"Course ID {course_id} not found in JSON")
     except (FileNotFoundError, json.JSONDecodeError) as e:
         print(f"Error loading JSON file: {e}")
         videos = []
-    return render(request, "course-detail/course_details.html", {"videos": videos})
+    return render(
+        request,
+        "course-detail/course_details.html",
+        {
+            "videos": videos,
+            "course_id": course_id,
+            "embed_urls": embed_urls,
+        },
+    )
